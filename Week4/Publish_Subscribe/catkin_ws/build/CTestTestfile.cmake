aa3ef39cd6041e002974bbe7857ef599b6181b4d
ShahdT <shahdaltalhi@gmail.com>
2020-07-15 00:28:54 +0300

    3.0

---
+++ Week4/Publish_Subscribe/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shahdt/catkin_ws/src
+# Build directory: /home/shahdt/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_tutorials")
---
+++ Week4/Publish_Subscribe/catkin_ws/build/robot_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shahdt/catkin_ws/src/robot_tutorials
+# Build directory: /home/shahdt/catkin_ws/build/robot_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
